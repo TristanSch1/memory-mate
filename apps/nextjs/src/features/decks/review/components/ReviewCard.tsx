5109e0ee5b2aa9eccc52d8f4e96c886ba4d5665a
Tristan Schwartz <tristan@lesentrecodeurs.com>
2023-08-06 23:17:01 +0200

    feat(nextjs): flip animation for review card

--- apps/nextjs/src/features/decks/review/components/ReviewCard.tsx
+++ apps/nextjs/src/features/decks/review/components/ReviewCard.tsx
@@ -1,15 +1,35 @@
-import { Card as GenericCard } from "@/components/ui/card";
 import { type TCard } from "@/features/cards/types";
+import { clsx } from "clsx";
 
 type Props = {
   card: TCard;
   isFlipped: boolean;
 };
+
 export const ReviewCard = ({ card, isFlipped }: Props) => {
   return (
-    <GenericCard
-      className={"h-[60vh] w-full select-none p-4 sm:p-6"}
-      dangerouslySetInnerHTML={{ __html: isFlipped ? card.back : card.front }}
-    />
+    <div
+      className={
+        "perspective h-[60vh] w-full select-none bg-transparent transition-all duration-300"
+      }
+    >
+      <div
+        className={clsx(
+          "preserve-3d relative h-full w-full transition-transform duration-1000",
+          {
+            "rotate-y-180": isFlipped,
+          },
+        )}
+      >
+        <div
+          className={"flip-card-front bg-white"}
+          dangerouslySetInnerHTML={{ __html: card.front }}
+        />
+        <div
+          className={"flip-card-back rotate-y-180 bg-neutral-100"}
+          dangerouslySetInnerHTML={{ __html: card.back }}
+        />
+      </div>
+    </div>
   );
 };
