cc8d523d3db5c5e84105f0c342c98a2385052819
Tristan Schwartz <tristan@lesentrecodeurs.com>
2023-08-12 14:03:43 +0200

    feat(api): folder router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -3,10 +3,12 @@ import { cardRouter } from "./router/card";
 import { cardReviewRouter } from "./router/cardReview";
 import { deckRouter } from "./router/deck";
 import { deckReviewRouter } from "./router/deckReview";
+import { folderRouter } from "./router/folder";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   deck: deckRouter,
+  folder: folderRouter,
   card: cardRouter,
   auth: authRouter,
   deckReview: deckReviewRouter,
